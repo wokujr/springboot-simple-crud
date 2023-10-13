5574bf39d29aac3391469777abfc134db829a116
wokujr <wokujr@gmail.com>
2023-10-14 01:08:45 +0700

    fix bug

--- src/main/java/com/employee/crud/EmployeeExcelExporter.java
+++ src/main/java/com/employee/crud/EmployeeExcelExporter.java
@@ -1,6 +1,7 @@
 package com.employee.crud;
 
 import com.employee.crud.entity.Employee;
+import com.employee.crud.repositoy.EmployeeRepository;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,17 +14,19 @@ import java.util.List;
 
 public class EmployeeExcelExporter {
 
+    private EmployeeRepository employeeRepository;
     private XSSFWorkbook workbook;
     private XSSFSheet sheet;
-    private List<Employee> EmployeeLists;
+    private List<Employee> employeeList;
 
-    public EmployeeExcelExporter(List<Employee> employeeLists) {
-        this.EmployeeLists = employeeLists;
+    public EmployeeExcelExporter( List<Employee> employeeLists) {
+        this.employeeList = employeeLists;
         workbook = new XSSFWorkbook();
         sheet = workbook.createSheet("Employee");
 
     }
 
+    //ROW Headers
     private void writeHeaderRow(){
 
         //First row is 0
@@ -43,11 +46,31 @@ public class EmployeeExcelExporter {
         cell = row.createCell(3);
         cell.setCellValue("Employee Email");
 
-
-
     }
-    private void writeDataRow(){
 
+    // Write Data To Each Header
+    private void writeDataRow(){
+        //Because we can have many data rows
+        int rowCount = 1;
+
+        for (Employee employee : employeeList){
+
+            Row row = sheet.createRow(rowCount);
+            Cell cell = row.createCell(0); //For Employee id
+            cell.setCellValue(employee.getId());
+
+            cell = row.createCell(1);
+            cell.setCellValue(employee.getFirstName());
+
+            cell = row.createCell(2);
+            cell.setCellValue(employee.getLastName());
+
+            cell = row.createCell(3);
+            cell.setCellValue(employee.getEmail());
+
+            //increment the row count to get all data
+            rowCount++;
+        }
     }
     public void export(HttpServletResponse response) throws IOException {
         writeHeaderRow();
